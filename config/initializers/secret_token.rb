71be6321e9e7979671b993e7f3794a3483842f36
PhamKhanh <phamkhanh90@gmail.com>
2013-11-05 21:35:04 +0700

    fafds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleBus::Application.config.secret_key_base = '1490c69c424a854a9bffe3ab63b8e901177b846f03924123228eb3c02c37dc7f01e0304f7d2f656c91bde1591d7ebf78080b14a637f2f275a993d622acd08180'
